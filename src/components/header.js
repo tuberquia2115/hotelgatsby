931307f0f32d8fdef3a86e32d8c5bdcdcecddd82
SOLUTRAFFICDEVELOPERBETA <solutrafficdev002@gmail.com>
2020-06-21 18:14:20 -0500

    ADD: pantalla de inicio y nosotros, integracion de plugins, gatsby-background-image, y configiguracion de la app

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,36 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React from 'react';
+import {css} from '@emotion/core'
+import Navegacion from './nav';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = () => {
+    return ( 
+       <header css={css`
+       background-color: #222;
+       padding: 1rem;
+       `}>
+           <div
+           css={css`
+           max-width: 1200px;
+           margin: 0 auto;
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+           @media (min-width: 768px){
+               display: flex;
+               align-items: center;
+               justify-content: space-between;
+           }
+           `}
+           >
+               <h1
+               css={css`
+               color: #fff;
+               text-align: center;
+               `}
+               >Hotel Gatsby</h1>
+
+               <Navegacion/>
+           </div>
+       </header>
+     );
 }
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+ 
+export default Header;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,58 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React from 'react'
+import Header from './header'
+import Helmet from 'react-helmet'
+import { Global, css } from '@emotion/core'
+import Footer from './footer'
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+const Layout = (props) => {
+    return (
+        <>
+            <Global
+                styles={css`
+        
+        html {
+            font-size: 62.5%;
         }
-      }
-    }
-  `)
+        body {
+            font-size: 18px;
+            font-size: 1.8rem;
+            line-height: 1.5;
+            font-family: 'PT Serif', sans-serif;
+        }
+        h1, h2, h3 {
+            margin: 0;
+            line-height: 1.5;
+        }
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+        h1, h2 {
+            font-family: 'Roboto', serif;
+        }
+        h3 {
+            font-family: 'PT Serif', sans-serif;
+        }
+
+        ul {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+        }
+        `}
+            />
+            <Helmet>
+                <title>Gatsby Hotel</title>
+                <link
+                    href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"
+                    rel="stylesheet" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;1,700&family=Roboto:wght@400;700&display=swap"
+                    rel="stylesheet" />
+            </Helmet>
+            <Header />
+            {props.children}
+
+            <Footer/>
+        </>
+    );
 }
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+export default Layout;
